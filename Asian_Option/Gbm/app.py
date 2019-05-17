1dc28108d007dca4a8fa364290b70ac321ba19b2
Laura di Stefano <distefanolaura46@gmail.com>
2019-05-17 20:58:44 +0100

    Delete Gbm Folder

--- Asian_Option/Gbm/app.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.urandom(24)
