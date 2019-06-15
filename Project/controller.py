87120f2ba01e0fbd545aa50c83fdce6323968e72
Laura di Stefano <distefanolaura46@gmail.com>
2019-06-15 18:52:21 +0200

    Basic frame for unified project

---
+++ Project/controller.py
@@ -0,0 +1,64 @@
+import os
+
+from flask import render_template, request, redirect, url_for
+from flask_login import LoginManager, current_user, \
+    login_user, logout_user, login_required
+
+from app import app
+from db_models import db, User
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user = current_user
+    return render_template("index.html", user=user)
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def create_login():
+    from forms import RegistrationForm
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User()
+        form.populate_obj(user)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+        # flash('Thanks for registering')
+        return redirect(url_for('index'))
+    return render_template("reg.html", form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    from forms import LoginForm
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = form.get_user()
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    if not os.path.isfile(os.path.join(os.path.dirname(__file__), 'sqlite.db')):
+        db.create_all()
+    app.run(debug=True)
---
+++ Project/db_models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import app
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    pw_hash = db.Column(db.String(80))
+    email = db.Column(db.String(120), nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def check_password(self, pw):
+        return check_password_hash(self.pw_hash, pw)
+
+    def set_password(self, pw):
+        self.pw_hash = generate_password_hash(pw)
+
+    def is_authenticated(self):
+        # Return True if the user is authenticated
+        return True
+
+    def is_active(self):
+        # True, as all users are active."""
+        return True
+
+    def is_anonymous(self):
+        # False, as anonymous users aren't supported.
+        return False
+
+    def get_id(self):
+        return self.id
