0c21a7dc712fbe62f0d016fcef0df4539ad23f6b
debo225 <diegodebortoli2018@outlook.it>
2019-09-18 17:27:44 +0200

    plots compute

--- Project/term_structure/compute.py
+++ Project/term_structure/compute.py
@@ -11,6 +11,14 @@ import numpy as np
 import pandas as pd
 from scipy import optimize
 from scipy.optimize import least_squares
+import numpy as np
+import bokeh.plotting as bp
+from bokeh.plotting import ColumnDataSource
+from bokeh.models import HoverTool
+from bokeh.layouts import row, column
+from bokeh.models import ColumnDataSource
+from bokeh.models.widgets import Slider, TextInput
+from bokeh.plotting import figure
 
 from term_structure.fit_df_CIR import fit_df_CIR
 from term_structure.fit_df_NelsonSiegel import fit_df_NelsonSiegel
@@ -93,4 +101,105 @@ def fitting_method(model, x0, data, flag1, flag2):
     spot_rate_model_error = (model_spot_rate - market_spot_rate) ** 2
 
     return market_discount_factor, market_spot_rate, model_discount_factor, model_spot_rate, \
-        discount_factor_model_error, spot_rate_model_error, param
+        discount_factor_model_error, spot_rate_model_error, param, ttm
+
+
+
+def discount_factor_term_structure(ttm, market_discount_factor, model_discount_factor):
+
+
+
+    data_market_discount_factor = ColumnDataSource(data=dict(
+        ttm=ttm,
+        market_discount_factor=market_discount_factor
+    ))
+
+    data_model_discount_factor = ColumnDataSource(data=dict(
+        ttm=ttm,
+        model_discount_factor=model_discount_factor
+    ))
+
+
+
+#    hover_market = HoverTool(attachment="below", names=['market discount factor'],
+#                             tooltips=[("Maturity", "@ttm"), ("market_discount_factor", "@market_discount_factor")])
+#
+#    hover_model = HoverTool(attachment="above", names=['model discount factor'], tooltips=[("Maturity", "@ttm"),
+#                            ("Model discount factor", "@model_discount_factor")])
+
+   
+
+    x_range = [min(ttm), max(ttm)]
+    y_range = [min(model_discount_factor), max(model_discount_factor)]
+
+    fig = bp.figure(tools=['save, pan, box_zoom, reset, crosshair'], x_range = x_range, y_range = y_range, title="Discount Factor Term Structure", plot_height=450,
+                    toolbar_location="left", x_axis_label='Time', y_axis_label='Discount Factor')
+
+    fig.line(x='ttm', y='model_discount_factor', source=data_model_discount_factor, color="#0095B6",
+             legend='Model Discount Factor Term Structure', line_width=4, alpha=0.8, name='Model discount term structure')
+
+
+    
+    fig.circle(x='ttm', y='market_discount_factor', source=data_market_discount_factor, color="#D21F1B",
+                legend='Market Discount Factor', size=6, name='Market discount factor')
+
+
+    fig.toolbar.active_drag = None
+    fig.legend.location = "bottom_left"
+    fig.legend.orientation = "vertical"
+
+    from bokeh.embed import components
+    script, div = components(fig)
+
+    return script, div
+    
+   
+
+def interest_rate_term_structure(ttm, market_spot_rate, model_spot_rate):
+
+
+
+    data_market_spot_rate = ColumnDataSource(data=dict(
+        ttm=ttm,
+        market_spot_rate=market_spot_rate
+    ))
+
+    data_model_spot_rate = ColumnDataSource(data=dict(
+        ttm=ttm,
+        model_spot_rate=model_spot_rate
+    ))
+
+
+
+#    hover_market = HoverTool(attachment="below", names=['market spot rate'],
+#                             tooltips=[("Maturity", "@ttm"), ("market_spot_rate", "@market_spot_rate")])
+#
+#    hover_model = HoverTool(attachment="above", names=['model spot rate'], tooltips=[("Maturity", "@ttm"),
+#                            ("Model spot rater", "@model_spot_rate")])
+
+   
+
+    x_range = [min(ttm), max(ttm)]
+    y_range = [min(model_spot_rate), max(model_spot_rate)]
+
+    fig = bp.figure(tools=['save, pan, box_zoom, reset, crosshair'], x_range = x_range, y_range = y_range, title="Spot Rate Term Structure", plot_height=450,
+                    toolbar_location="left", x_axis_label='Time', y_axis_label='Spot Rate')
+
+    fig.line(x='ttm', y='model_spot_rate', source=data_model_spot_rate, color="#0095B6",
+             legend='Model Spot Rate Term Structure', line_width=4, alpha=0.8, name='Model spot rate term structure')
+
+
+    
+    fig.circle(x='ttm', y='market_spot_rate', source=data_market_spot_rate, color="#D21F1B",
+                legend='Market spot rate', size=6, name='Market spot rate')
+
+
+    fig.toolbar.active_drag = None
+    fig.legend.location = "bottom_left"
+    fig.legend.orientation = "vertical"
+
+    from bokeh.embed import components
+    script, div = components(fig)
+
+    return script, div
+
