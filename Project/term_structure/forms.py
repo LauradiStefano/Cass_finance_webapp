f02e9dd05fd4570c7fce4cd9bace69308580db72
Laura di Stefano <distefanolaura46@gmail.com>
2019-10-05 16:15:59 +0200

    Changed default value in term strucuture

--- Project/term_structure/forms.py
+++ Project/term_structure/forms.py
@@ -8,8 +8,8 @@ class ComputeForm(wtf.Form):
                                                      ('2', 'Nelson Siegel'), ('3', 'Svensson')], default='0')
 
     name_parameters = {
-        '0': ['$$ \kappa $$', r'$$ \theta $$', '$$ \sigma $$', r'$$ \rho $$'],
-        '1': ['$$ \kappa $$', r'$$ \theta $$', '$$ \sigma $$', r'$$ \rho $$'],
+        '0': ['$$ \kappa $$', r'$$ \theta $$', '$$ \sigma $$', r'$$ v_{0} $$'],
+        '1': ['$$ \kappa $$', r'$$ \theta $$', '$$ \sigma $$', r'$$ v_{0} $$'],
         '2': [r'$$ \beta_{0} $$', r'$$ \beta_{1} $$', r'$$ \beta_{2} $$', r'$$ \tau $$'],
         '3': [r'$$ \beta_{0} $$', r'$$ \beta_{1} $$', r'$$ \beta_{2} $$', r'$$ \beta_{3} $$', r'$$ \tau_{1} $$',
               r'$$ \tau_{2} $$']}
@@ -17,33 +17,33 @@ class ComputeForm(wtf.Form):
     file_name = StringField(label='Name DataSet', validators=[InputRequired(), validators.Length(max=25)])
 
     # Vasicek distribution
-    kappa_vasicek = wtf.FloatField(label=name_parameters['0'][0], default=1,
+    kappa_vasicek = wtf.FloatField(label=name_parameters['0'][0], default=0.1,
                                    validators=[InputRequired()])
-    theta_vasicek = wtf.FloatField(label=name_parameters['0'][1], default=1,
+    theta_vasicek = wtf.FloatField(label=name_parameters['0'][1], default=0.05,
                                    validators=[InputRequired()])
-    sigma_vasicek = wtf.FloatField(label=name_parameters['0'][2], default=1,
+    sigma_vasicek = wtf.FloatField(label=name_parameters['0'][2], default=0.03,
                                    validators=[InputRequired()])
-    rho_vasicek = wtf.FloatField(label=name_parameters['0'][3], default=1,
+    rho_vasicek = wtf.FloatField(label=name_parameters['0'][3], default=0.04,
                                  validators=[InputRequired()])
 
     # Cir distribution
-    kappa_cir = wtf.FloatField(label=name_parameters['1'][0], default=1,
+    kappa_cir = wtf.FloatField(label=name_parameters['1'][0], default=0.1,
                                validators=[InputRequired()])
-    theta_cir = wtf.FloatField(label=name_parameters['1'][1], default=1,
+    theta_cir = wtf.FloatField(label=name_parameters['1'][1], default=0.05,
                                validators=[InputRequired()])
-    sigma_cir = wtf.FloatField(label=name_parameters['1'][2], default=1,
+    sigma_cir = wtf.FloatField(label=name_parameters['1'][2], default=0.03,
                                validators=[InputRequired()])
-    rho_cir = wtf.FloatField(label=name_parameters['1'][3], default=1,
+    rho_cir = wtf.FloatField(label=name_parameters['1'][3], default=0.04,
                              validators=[InputRequired()])
 
     # Nelson Siegel
-    beta0_nelson = wtf.FloatField(label=name_parameters['2'][0], default=1,
+    beta0_nelson = wtf.FloatField(label=name_parameters['2'][0], default=0.02,
                                   validators=[InputRequired()])
-    beta1_nelson = wtf.FloatField(label=name_parameters['2'][1], default=1,
+    beta1_nelson = wtf.FloatField(label=name_parameters['2'][1], default=0.01,
                                   validators=[InputRequired()])
-    beta2_nelson = wtf.FloatField(label=name_parameters['2'][2], default=1,
+    beta2_nelson = wtf.FloatField(label=name_parameters['2'][2], default=0.5,
                                   validators=[InputRequired()])
-    tau_nelson = wtf.FloatField(label=name_parameters['2'][3], default=1,
+    tau_nelson = wtf.FloatField(label=name_parameters['2'][3], default=2.3,
                                 validators=[InputRequired()])
 
     # Svensson distribution
@@ -65,7 +65,7 @@ class ComputeForm(wtf.Form):
 
     discount_factor = wtf.RadioField(label='Discount Factor-Spot Rate', choices=[('0', 'Spot Rate'),
                                                                                  ('1', 'Discount Factor')], default='0')
-    least_fmin = wtf.RadioField(label='Optimization Method', choices=[('0', 'Least Squares'), ('1', 'Fmin')],
+    least_fmin = wtf.RadioField(label='Optimization Method', choices=[('0', 'No Linear Least Squares'), ('1', 'Fmin')],
                                 default='0')
 
     button_compute = wtf.SubmitField(label='Compute')
