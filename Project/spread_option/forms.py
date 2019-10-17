123a754963aa74c4a1f322d2d44096a1231a0900
debo225 <diegodebortoli2018@outlook.it>
2019-10-17 15:23:17 +0200

    Fix inputs form

--- Project/spread_option/forms.py
+++ Project/spread_option/forms.py
@@ -8,9 +8,9 @@ class ComputeForm(wtf.Form):
     price_2 = wtf.FloatField(label='Spot Price', default=96,
                              validators=[wtf.validators.InputRequired(), validators.NumberRange(0, 1E+20)])
 
-    dividend_yield_1 = wtf.FloatField(label='Dividend Yield \((\%) \)', default=0.05,
+    dividend_yield_1 = wtf.FloatField(label='Dividend Yield \((\%) \)', default= 5,
                                       validators=[wtf.validators.InputRequired()])
-    dividend_yield_2 = wtf.FloatField(label='Dividend Yield \((\%) \)', default=0.05,
+    dividend_yield_2 = wtf.FloatField(label='Dividend Yield \((\%) \)', default= 5,
                                       validators=[wtf.validators.InputRequired()])
 
     volatility_1 = wtf.FloatField(label='$$ \sigma $$', default=0.2,
@@ -23,10 +23,10 @@ class ComputeForm(wtf.Form):
                             validators=[wtf.validators.InputRequired(), validators.NumberRange(0, 1E+20)])
     risk_free = wtf.FloatField(label='Interest Rate \((\%) \)', default=10, validators=[wtf.validators.InputRequired()])
 
-    time = wtf.FloatField(label='Time to Matutity', default=0.5,
+    time = wtf.FloatField(label='Time to Matutity', default= 1,
                           validators=[wtf.validators.InputRequired(), validators.NumberRange(0, 1E+20)])
 
-    rho = wtf.FloatField(label=r'$$ \rho $$', default=0.1,
+    rho = wtf.FloatField(label=r'$$ \rho $$', default= 0.5,
                          validators=[wtf.validators.InputRequired()])
 
     dump = wtf.FloatField(label='Dump Parameter', default=0.75,
