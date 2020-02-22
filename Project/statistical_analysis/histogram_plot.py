920a56f25fb58b778a6203e756b8732e649296e5
Laura di Stefano <distefanolaura46@gmail.com>
2020-02-22 18:47:42 +0100

    Formaatted text

--- Project/statistical_analysis/histogram_plot.py
+++ Project/statistical_analysis/histogram_plot.py
@@ -4,51 +4,39 @@ Created on Tue Dec  3 21:03:47 2019
 
 @author: Diego
 """
-import pandas as pd
 import bokeh.plotting as bp
-from bokeh.plotting import ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.io import curdoc
-from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider, TextInput
-from bokeh.plotting import figure
-from bokeh.core.properties import value
-import scipy.stats 
-import math
 import numpy as np
+import scipy.stats
+from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
 
 
-
-def histogram_distribution(log_returns):
-    
-    hist,edges = np.histogram(log_returns, density = True, bins=100)
-    m = np.mean(log_returns) 
+def create_histogram_distribution(log_returns):
+    hist, edges = np.histogram(log_returns, density=True, bins=100)
+    m = np.mean(log_returns)
     sg = np.std(log_returns)
-    log_returns.sort(reverse = False)
-    normal_pdf = lambda x : scipy.stats.norm.pdf (x, m, sg)
-    norm_pdf= [normal_pdf(i) for i in log_returns] #aggiungere all'output
-    
-    #log_returns.sort(reverse = False) 
-    
-    
+    log_returns.sort(reverse=False)
+    normal_pdf = lambda x: scipy.stats.norm.pdf(x, m, sg)
+    norm_pdf = [normal_pdf(i) for i in log_returns]  # aggiungere all'output
+
+    # log_returns.sort(reverse = False)
+
     data_hist = ColumnDataSource(data=dict(
         hist=hist,
         edges_left=edges[:-1],
         edges_right=edges[1:]))
-    
+
     data_theoretical = ColumnDataSource(data=dict(
         log_returns=log_returns,
         norm_pdf=norm_pdf))
 
     hover_histogram = HoverTool(attachment="above", names=['histogram'],
-                                      tooltips=[("edges", "@edges"),
-                                                ("hist", "@histogram")])
-    
-    hover_theoretical = HoverTool(attachment="above", names=['discount factor'],
-                                      tooltips=[("log_returns", "@log returns"),
-                                                ("norm_pdf", "@norm pdf")])
+                                tooltips=[("edges", "@edges"),
+                                          ("hist", "@histogram")])
 
+    hover_theoretical = HoverTool(attachment="above", names=['discount factor'],
+                                  tooltips=[("log_returns", "@log returns"),
+                                            ("norm_pdf", "@norm pdf")])
 
     fig = bp.figure(tools=['save, pan, box_zoom, reset, crosshair', hover_histogram, hover_theoretical],
                     sizing_mode='scale_both', toolbar_location="right", x_axis_label='Series of Data',
@@ -56,18 +44,15 @@ def histogram_distribution(log_returns):
 
     fig.quad(top='hist', bottom=0, left='edges_left', right='edges_right', source=data_hist,
              color="#0095B6", line_color="#ffffff", alpha=1, name='Histogram Log Returns')
-    
-    fig.line(x='log_returns', y='norm_pdf', source=data_theoretical, color="#D21F1B", legend='Log returns PDF', line_width=1, alpha=1, name='Theoretical Distribution')
 
+    fig.line(x='log_returns', y='norm_pdf', source=data_theoretical, color="#D21F1B", legend='Log returns PDF',
+             line_width=1, alpha=1, name='Theoretical Distribution')
 
     fig.toolbar.active_drag = None
 
     from bokeh.embed import components
     script, div = components(fig)
-    
-    
+
     return fig, div
 
-
-
-curdoc().add_root(row(p , width=800))
+# curdoc().add_root(row(p, width=800))
--- Project/statistical_analysis/qq_plot.py
+++ Project/statistical_analysis/qq_plot.py
@@ -1,66 +1,53 @@
-import pandas as pd
 import bokeh.plotting as bp
-from bokeh.plotting import ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.io import curdoc
-from bokeh.layouts import row, column
+import scipy.stats
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider, TextInput
-from bokeh.plotting import figure
-from bokeh.core.properties import value
-import scipy.stats 
-import math
+from bokeh.models import HoverTool
 
 
+def qq_plot(log_returns):
+    (x, empirical_distr), (slope, inter, cor) = scipy.stats.probplot(log_returns, dist="norm")  # , plot=pylab)
 
-
-def qq_plot(log_retruns):
-    
-    (x,empirical_distr), (slope, inter, cor) =scipy.stats.probplot(log_returns, dist="norm")#, plot=pylab)
-    
     teoretical_quantiles = x
-    #osmf = x.take([0, -1])  # endpoints
+    # osmf = x.take([0, -1])  # endpoints
     normal_distr = slope * teoretical_quantiles + inter
-    
-    #normal_empirical = qq_plot_data[0]
+
+    # normal_empirical = qq_plot_data[0]
     # teoretical_quantiles = normal_empirical[0]
     # empirical_distr = normal_empirical[1]
-    
-    
+
     data = ColumnDataSource(data=dict(
         teoretical_quantiles=teoretical_quantiles,
         normal_distr=normal_distr,
         empirical_distr=empirical_distr
     ))
-    
+
     hover_normal = HoverTool(attachment="above", names=['Normal Distr'],
-                             tooltips=[("teoretical quantiles", "@teoretical_quantiles"), ("Normal Distr", "@normal_distr")])
-    
+                             tooltips=[("teoretical quantiles", "@teoretical_quantiles"),
+                                       ("Normal Distr", "@normal_distr")])
+
     hover_empirical = HoverTool(attachment="below", names=['model spot rate'],
-                            tooltips=[("teoretical quantiles", "@teoretical_quantiles"), ("Empirical Distr", "@empirical_distr")])
-    
-    x_range = [min(teoretical_quantiles)-1, max(teoretical_quantiles) + 1]
-    y_range = [min(empirical_distr)-0.02, max(empirical_distr) + 0.02]
-    
+                                tooltips=[("teoretical quantiles", "@teoretical_quantiles"),
+                                          ("Empirical Distr", "@empirical_distr")])
+
+    x_range = [min(teoretical_quantiles) - 1, max(teoretical_quantiles) + 1]
+    y_range = [min(empirical_distr) - 0.02, max(empirical_distr) + 0.02]
+
     fig = bp.figure(tools=['save, pan, box_zoom, reset, crosshair', hover_empirical, hover_normal], x_range=x_range,
                     y_range=y_range, sizing_mode='scale_both', toolbar_location="right",
                     x_axis_label='Quantiles',
                     y_axis_label=' Distribution')
-    
-    fig.circle(x='teoretical_quantiles', y='empirical_distr', source=data, color="#0095B6", legend='Empirical Distribution',
-            size=6, name='Normal Distribution')
-    
-    fig.line(x='teoretical_quantiles', y='normal_distr', source=data, color="#D21F1B", legend='Normal Distribution', line_width=4, alpha=0.8, name='Empirical Distr')
-    
-    
+
+    fig.circle(x='teoretical_quantiles', y='empirical_distr', source=data, color="#0095B6",
+               legend='Empirical Distribution',
+               size=6, name='Normal Distribution')
+
+    fig.line(x='teoretical_quantiles', y='normal_distr', source=data, color="#D21F1B", legend='Normal Distribution',
+             line_width=4, alpha=0.8, name='Empirical Distr')
+
     fig.toolbar.active_drag = None
     fig.legend.location = "bottom_right"
-    
+
     from bokeh.embed import components
     script, div = components(fig)
-    
-    
+
     return fig, div
-
-
-
