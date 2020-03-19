506e61ee3fc56fd6d2d15f2b9fad8a0b1060ce94
Laura di Stefano <distefanolaura46@gmail.com>
2020-03-19 10:31:11 +0100

    Formatted text

--- Project/term_structure/get_dates.py
+++ Project/term_structure/get_dates.py
@@ -9,45 +9,39 @@ import datetime
 
 
 def list_of_daily_dates(time):
-    
     last_time = time[-1]
-    if last_time>=1:
-        n_day_years = last_time*365
-        #n_excess_day = (last_time - int(last_time))*365
-        #n_day_from_today = int(round(n_day_years+n_excess_day,0))
-        n_day_from_today=int(n_day_years)
-    else :
-        n_day_from_today = int(round(last_time*365,0))
-        
-    
-    dates=[]
+    if last_time >= 1:
+        n_day_years = last_time * 365
+        # n_excess_day = (last_time - int(last_time))*365
+        # n_day_from_today = int(round(n_day_years+n_excess_day,0))
+        n_day_from_today = int(n_day_years)
+    else:
+        n_day_from_today = int(round(last_time * 365, 0))
+
+    dates = []
     dates.append(datetime.date.today())
-    
-    for i in  range(0, n_day_from_today):
-        
-        next_day_date = dates[i]+datetime.timedelta(days=1)
+
+    for i in range(0, n_day_from_today):
+        next_day_date = dates[i] + datetime.timedelta(days=1)
         dates.append(next_day_date)
-        
-    
-    
-    annual_basis_date =[]
-    annual_basis_date.append(1/365)
-    for i in  range(0, n_day_from_today-1):
-        
-        next_annual_basis_date = annual_basis_date[i]+1/365
+
+    annual_basis_date = []
+    annual_basis_date.append(1 / 365)
+    for i in range(0, n_day_from_today - 1):
+        next_annual_basis_date = annual_basis_date[i] + 1 / 365
         annual_basis_date.append(next_annual_basis_date)
-    
+
     annual_basis_date.insert(0, 0)
 
-    listed_dates =[]
-    for i in range(0, n_day_from_today+1):
-        next_day_date=dates[i]
+    listed_dates = []
+    for i in range(0, n_day_from_today + 1):
+        next_day_date = dates[i]
         string_date = next_day_date.strftime('%m/%d/%Y')
         listed_dates.append(string_date)
-    
+
     return listed_dates, annual_basis_date
-    
-    for i in range(0, n_day_from_today+1):
+
+    for i in range(0, n_day_from_today + 1):
         next_day_date = dates[i]
         string_date = next_day_date.strftime('%m/%d/%Y')
         listed_dates.append(string_date)
