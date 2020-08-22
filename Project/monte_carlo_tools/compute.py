6b84f7106f0f60b024b3bc7f4d72a9474d457153
LauradiStefano <distefanolaura46@gmail.com>
2020-08-22 14:57:25 +0200

    Enabled 3d plot

--- Project/monte_carlo_tools/compute.py
+++ Project/monte_carlo_tools/compute.py
@@ -15,11 +15,14 @@ from monte_carlo_tools.get_mc_vg import get_mc_vg
 
 from monte_carlo_tools.get_moments import get_moments
 from scipy.stats import skew, kurtosis
-from bokeh.plotting import ColumnDataSource
+from bokeh.models import ColumnDataSource, LayoutDOM
 from bokeh.models import HoverTool
 from bokeh.layouts import gridplot
 from bokeh.palettes import Spectral11
 from bokeh.models import BoxAnnotation
+from bokeh.core.properties import Instance, String
+from bokeh.util.compiler import TypeScript
+from bokeh.embed import components
 
 import bokeh.plotting as bp
 import numpy as np
@@ -35,17 +38,14 @@ import itertools
 """
 
 
-# form.price_abm.data, form.price_gbm.data,
-# form.price_cir.data, form.price_mrg.data, form.price_heston.data
-
-def get_simutalted_path_and_moments(T, NStep, NPaths, model, mu_abm, sigma_abm, mu_cir, alpha_cir, sigma_cir, mu_dejd,
-                                    sigma_dejd, lambda_dejd, p_dejd, eta1_dejd, eta2_dejd, mu_ewma, v0_ewma, omega_ewma,
-                                    alpha_ewma, beta_ewma, asymm_ewma, mu_garch, v0_garch, omega_garch, alpha_garch,
-                                    beta_garch, asymm_garch, mu_gbm, sigma_gbm, mu_heston, v0_heston, alpha_heston,
-                                    beta_heston, eta_heston, rho_heston, mu_mjd, sigma_mjd, lambda_mjd, mu_jumps_mjd,
-                                    sg_jumps_mjd, mu_gmr, alpha_gmr, sigma_gmr, theta_vg, sigma_vg, kappa_vg,
-                                    price_abm, price_cir, price_dejd, price_ewma, price_garch, price_gbm, price_heston,
-                                    price_mjd, price_mrg, price_vg):
+def get_simulated_path_and_moments(T, NStep, NPaths, model, mu_abm, sigma_abm, mu_cir, alpha_cir, sigma_cir, mu_dejd,
+                                   sigma_dejd, lambda_dejd, p_dejd, eta1_dejd, eta2_dejd, mu_ewma, v0_ewma, omega_ewma,
+                                   alpha_ewma, beta_ewma, asymm_ewma, mu_garch, v0_garch, omega_garch, alpha_garch,
+                                   beta_garch, asymm_garch, mu_gbm, sigma_gbm, mu_heston, v0_heston, alpha_heston,
+                                   beta_heston, eta_heston, rho_heston, mu_mjd, sigma_mjd, lambda_mjd, mu_jumps_mjd,
+                                   sg_jumps_mjd, mu_gmr, alpha_gmr, sigma_gmr, theta_vg, sigma_vg, kappa_vg,
+                                   price_abm, price_cir, price_dejd, price_ewma, price_garch, price_gbm, price_heston,
+                                   price_mjd, price_mrg, price_vg):
     """1 - Fix steps, nsimulations, time horizon and the model"""
     # T=5
     # NStep=50
@@ -243,9 +243,7 @@ def create_plot_simulated_paths(simulated_paths, timestep, NStep, NPaths, quanti
 
     figure = gridplot([[fig_simulated_1, fig_simulated_2]], sizing_mode='scale_both')
 
-    from bokeh.embed import components
     script, div = components(figure)
-
     return script, div
 
 
@@ -374,7 +372,6 @@ def create_plot_moments(simulated_paths, moments, timestep):
     figure = gridplot([[fig_exp_value, fig_variance_value], [fig_skewness_value, fig_kurtosis_value]],
                       sizing_mode='scale_both')
 
-    from bokeh.embed import components
     script, div = components(figure)
     return script, div
 
@@ -420,7 +417,218 @@ def create_plot_histogram(simulated_paths, NStep):
 
     figure = gridplot([[fig_hist_1, fig_hist_2], [fig_hist_3]], sizing_mode='scale_both')
 
-    from bokeh.embed import components
     script, div = components(figure)
+    return script, div
 
+
+def create_plot_3D_evolution_of_density(simulated_paths, moments, timestep, NStep, NPaths):
+    TS_CODE = """
+    // This custom model wraps one part of the third-party vis.js library:
+    //
+    //     http://visjs.org/index.html
+    //
+    // Making it easy to hook up python data analytics tools (NumPy, SciPy,
+    // Pandas, etc.) to web presentations using the Bokeh server.
+
+    import {LayoutDOM, LayoutDOMView} from "models/layouts/layout_dom"
+    import {ColumnDataSource} from "models/sources/column_data_source"
+    import {LayoutItem} from "core/layout"
+    import * as p from "core/properties"
+
+    declare namespace vis {
+      class Graph3d {
+        constructor(el: HTMLElement, data: object, OPTIONS: object)
+        setData(data: vis.DataSet): void
+      }
+
+      class DataSet {
+        add(data: unknown): void
+      }
+    }
+
+    // This defines some default options for the Graph3d feature of vis.js
+    // See: http://visjs.org/graph3d_examples.html for more details.
+    const OPTIONS = {
+      width: '100%',
+      height: '600px',
+      style: 'surface',
+      showPerspective: true,
+      showGrid: true,
+      keepAspectRatio: true,
+      verticalRatio: 0.5,
+      legendLabel: 'stuff',
+      cameraPosition: {
+        horizontal: 1.0,
+        vertical: 0.5,
+        distance: 1.7,
+      },
+    }
+    // To create custom model extensions that will render on to the HTML canvas
+    // or into the DOM, we must create a View subclass for the model.
+    //
+    // In this case we will subclass from the existing BokehJS ``LayoutDOMView``
+    export class Surface3dView extends LayoutDOMView {
+      model: Surface3d
+
+      private _graph: vis.Graph3d
+
+      initialize(): void {
+        super.initialize()
+
+        const url = "https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis.min.js"
+        const script = document.createElement("script")
+        script.onload = () => this._init()
+        script.async = false
+        script.src = url
+        document.head.appendChild(script)
+      }
+
+      private _init(): void {
+        // Create a new Graph3s using the vis.js API. This assumes the vis.js has
+        // already been loaded (e.g. in a custom app template). In the future Bokeh
+        // models will be able to specify and load external scripts automatically.
+        //
+        // BokehJS Views create <div> elements by default, accessible as this.el.
+        // Many Bokeh views ignore this default <div>, and instead do things like
+        // draw to the HTML canvas. In this case though, we use the <div> to attach
+        // a Graph3d to the DOM.
+        this._graph = new vis.Graph3d(this.el, this.get_data(), OPTIONS)
+
+        // Set a listener so that when the Bokeh data source has a change
+        // event, we can process the new data
+        this.connect(this.model.data_source.change, () => {
+          this._graph.setData(this.get_data())
+        })
+      }
+
+      // This is the callback executed when the Bokeh data has an change. Its basic
+      // function is to adapt the Bokeh data source to the vis.js DataSet format.
+      get_data(): vis.DataSet {
+        const data = new vis.DataSet()
+        const source = this.model.data_source
+        for (let i = 0; i < source.get_length()!; i++) {
+          data.add({
+            x: source.data[this.model.x][i],
+            y: source.data[this.model.y][i],
+            z: source.data[this.model.z][i],
+          })
+        }
+        return data
+      }
+
+      get child_models(): LayoutDOM[] {
+        return []
+      }
+
+      _update_layout(): void {
+        this.layout = new LayoutItem()
+        this.layout.set_sizing(this.box_sizing())
+      }
+    }
+
+    // We must also create a corresponding JavaScript BokehJS model subclass to
+    // correspond to the python Bokeh model subclass. In this case, since we want
+    // an element that can position itself in the DOM according to a Bokeh layout,
+    // we subclass from ``LayoutDOM``
+    export namespace Surface3d {
+      export type Attrs = p.AttrsOf<Props>
+
+      export type Props = LayoutDOM.Props & {
+        x: p.Property<string>
+        y: p.Property<string>
+        z: p.Property<string>
+        data_source: p.Property<ColumnDataSource>
+      }
+    }
+
+    export interface Surface3d extends Surface3d.Attrs {}
+
+    export class Surface3d extends LayoutDOM {
+      properties: Surface3d.Props
+      __view_type__: Surface3dView
+
+      constructor(attrs?: Partial<Surface3d.Attrs>) {
+        super(attrs)
+      }
+
+      // The ``__name__`` class attribute should generally match exactly the name
+      // of the corresponding Python class. Note that if using TypeScript, this
+      // will be automatically filled in during compilation, so except in some
+      // special cases, this shouldn't be generally included manually, to avoid
+      // typos, which would prohibit serialization/deserialization of this model.
+      static __name__ = "Surface3d"
+
+      static init_Surface3d() {
+        // This is usually boilerplate. In some cases there may not be a view.
+        this.prototype.default_view = Surface3dView
+
+        // The @define block adds corresponding "properties" to the JS model. These
+        // should basically line up 1-1 with the Python model class. Most property
+        // types have counterparts, e.g. ``bokeh.core.properties.String`` will be
+        // ``p.String`` in the JS implementatin. Where the JS type system is not yet
+        // as rich, you can use ``p.Any`` as a "wildcard" property type.
+        this.define<Surface3d.Props>({
+          x:            [ p.String   ],
+          y:            [ p.String   ],
+          z:            [ p.String   ],
+          data_source:  [ p.Instance ],
+        })
+      }
+    }
+    """
+
+    # This custom extension model will have a DOM view that should layout-able in
+    # Bokeh layouts, so use ``LayoutDOM`` as the base class. If you wanted to create
+    # a custom tool, you could inherit from ``Tool``, or from ``Glyph`` if you
+    # wanted to create a custom glyph, etc.
+    class Surface3d(LayoutDOM):
+        # The special class attribute ``__implementation__`` should contain a string
+        # of JavaScript code that implements the browser side of the extension model.
+        __implementation__ = TypeScript(TS_CODE)
+
+        # Below are all the "properties" for this model. Bokeh properties are
+        # class attributes that define the fields (and their types) that can be
+        # communicated automatically between Python and the browser. Properties
+        # also support type validation. More information about properties in
+        # can be found here:
+        #
+        #    https://docs.bokeh.org/en/latest/docs/reference/core/properties.html#bokeh-core-properties
+
+        # This is a Bokeh ColumnDataSource that can be updated in the Bokeh
+        # server by Python code
+        data_source = Instance(ColumnDataSource)
+
+        # The vis.js library that we are wrapping expects data for x, y, and z.
+        # The data will actually be stored in the ColumnDataSource, but these
+        # properties let us specify the *name* of the column that should be
+        # used for each field.
+        x = String
+
+        y = String
+
+        z = String
+
+    mm = np.quantile(simulated_paths[-1], 0.01)
+    mx = np.quantile(simulated_paths[-1], 0.99)
+    nxgrid = 20
+    timestep = np.asarray(timestep)
+    timestep.shape = (len(timestep), 1)
+    output = np.hstack((timestep, simulated_paths))
+    [Xgrid, Tgrid] = np.meshgrid(np.linspace(mm, mx, nxgrid), output[1:-1, 0])
+    z = np.zeros((NStep - 1, nxgrid))
+    dx = Xgrid[0, 1] - Xgrid[0, 0]
+
+    for j in range(1, NStep - 1):
+        N, ccc = np.histogram(simulated_paths[j, :], np.linspace(mm, mx, nxgrid + 1))
+        z[j, :] = N / (NPaths * dx)
+
+    data = ColumnDataSource(data=dict(
+        x=Xgrid,
+        y=Tgrid,
+        z=z
+    ))
+
+    fig = Surface3d(x="x", y="y", z="z", data_source=data, height=600, width=600 )
+
+    script, div = components(fig)
     return script, div
--- Project/monte_carlo_tools/controller.py
+++ Project/monte_carlo_tools/controller.py
@@ -8,8 +8,8 @@ from sqlalchemy import desc
 
 from db_models import db
 from db_models import monte_carlo_tools as compute
-from monte_carlo_tools.compute import get_simutalted_path_and_moments, create_plot_simulated_paths, create_plot_moments, \
-    create_plot_histogram
+from monte_carlo_tools.compute import get_simulated_path_and_moments, create_plot_simulated_paths, create_plot_moments, \
+    create_plot_histogram, create_plot_3D_evolution_of_density
 from monte_carlo_tools.forms import ComputeForm
 
 
@@ -24,29 +24,31 @@ def controller_monte_carlo_tools(user, request):
 
     plot_histogram = None
 
+    plot_3d_density = None
+
     if request.method == "POST":
         if form.validate():
             simulated_paths, moments, quantiles, timestep = \
-                get_simutalted_path_and_moments(form.time.data, form.number_step.data, form.number_paths.data,
-                                                form.model_choice.data, form.mu_abm.data, form.sigma_abm.data,
-                                                form.mu_cir.data, form.alpha_cir.data, form.sigma_cir.data,
-                                                form.mu_dejd.data, form.sigma_dejd.data, form.lam_dejd.data,
-                                                form.rho_dejd.data, form.eta1_dejd.data, form.eta2_dejd.data,
-                                                form.mu_ewma.data, form.volatility_t0_ewma.data, form.omega_ewma.data,
-                                                form.alpha_ewma.data, form.beta_ewma.data, form.asymm_ewma.data,
-                                                form.mu_garch.data, form.volatility_t0_garch.data,
-                                                form.omega_garch.data, form.alpha_garch.data, form.beta_garch.data,
-                                                form.asymm_garch.data, form.mu_gbm.data, form.sigma_gbm.data,
-                                                form.mu_heston.data, form.volatility_t0_heston.data,
-                                                form.alpha_heston.data, form.beta_heston.data, form.eta_heston.data,
-                                                form.rho_heston.data, form.mu_mjd.data, form.sigma_mjd.data,
-                                                form.lam_mjd.data, form.mu_x_mjd.data, form.sigma_x_mjd.data,
-                                                form.mu_mrg.data, form.alpha_mrg.data, form.sigma_mrg.data,
-                                                form.theta_vg.data, form.sigma_vg.data, form.kappa_vg.data,
-                                                form.price_abm.data, form.price_cir.data, form.price_dejd.data,
-                                                form.price_ewma.data, form.price_garch.data, form.price_gbm.data,
-                                                form.price_heston.data, form.price_mjd.data, form.price_mrg.data,
-                                                form.price_vg.data)
+                get_simulated_path_and_moments(form.time.data, form.number_step.data, form.number_paths.data,
+                                               form.model_choice.data, form.mu_abm.data, form.sigma_abm.data,
+                                               form.mu_cir.data, form.alpha_cir.data, form.sigma_cir.data,
+                                               form.mu_dejd.data, form.sigma_dejd.data, form.lam_dejd.data,
+                                               form.rho_dejd.data, form.eta1_dejd.data, form.eta2_dejd.data,
+                                               form.mu_ewma.data, form.volatility_t0_ewma.data, form.omega_ewma.data,
+                                               form.alpha_ewma.data, form.beta_ewma.data, form.asymm_ewma.data,
+                                               form.mu_garch.data, form.volatility_t0_garch.data,
+                                               form.omega_garch.data, form.alpha_garch.data, form.beta_garch.data,
+                                               form.asymm_garch.data, form.mu_gbm.data, form.sigma_gbm.data,
+                                               form.mu_heston.data, form.volatility_t0_heston.data,
+                                               form.alpha_heston.data, form.beta_heston.data, form.eta_heston.data,
+                                               form.rho_heston.data, form.mu_mjd.data, form.sigma_mjd.data,
+                                               form.lam_mjd.data, form.mu_x_mjd.data, form.sigma_x_mjd.data,
+                                               form.mu_mrg.data, form.alpha_mrg.data, form.sigma_mrg.data,
+                                               form.theta_vg.data, form.sigma_vg.data, form.kappa_vg.data,
+                                               form.price_abm.data, form.price_cir.data, form.price_dejd.data,
+                                               form.price_ewma.data, form.price_garch.data, form.price_gbm.data,
+                                               form.price_heston.data, form.price_mjd.data, form.price_mrg.data,
+                                               form.price_vg.data)
 
             plot_simulated_paths = create_plot_simulated_paths(simulated_paths, timestep, form.number_step.data,
                                                                form.number_paths.data, quantiles)
@@ -54,6 +56,9 @@ def controller_monte_carlo_tools(user, request):
             plot_moments = create_plot_moments(simulated_paths, moments, timestep)
 
             plot_histogram = create_plot_histogram(simulated_paths, form.number_step.data)
+
+            plot_3d_density = create_plot_3D_evolution_of_density(simulated_paths, moments, timestep,
+                                                                  form.number_step.data, form.number_paths.data)
 
             if user.is_authenticated:  # store data in db
                 object = compute()
@@ -90,8 +95,11 @@ def controller_monte_carlo_tools(user, request):
 
                 plot_histogram = create_plot_histogram(simulated_paths, form.number_step.data)
 
+                plot_3d_density = create_plot_3D_evolution_of_density(simulated_paths, moments, timestep,
+                                                                      form.number_step.data, form.number_paths.data)
+
     return {'form': form, 'user': user, 'plot_simulated_paths': plot_simulated_paths, 'plot_moments': plot_moments,
-            'plot_histogram': plot_histogram, 'sim_id': sim_id}
+            'plot_histogram': plot_histogram, 'plot_3d_density': plot_3d_density, 'sim_id': sim_id}
 
 
 def populate_form_from_instance(instance):
@@ -124,9 +132,11 @@ def controller_old_monte_carlo_tools(user):
 
             plot_histogram = create_plot_histogram(simulated_paths, form.number_step.data)
 
+            plot_3d_density = create_plot_3D_evolution_of_density(simulated_paths, moments, timestep,
+                                                                  form.number_step.data, form.number_paths.data)
             data.append(
                 {'form': form, 'id': id, 'plot_simulated_paths': plot_simulated_paths, 'plot_moments': plot_moments,
-                 'plot_histogram': plot_histogram})
+                 'plot_histogram': plot_histogram, 'plot_3d_density': plot_3d_density})
 
     return {'data': data}
 
